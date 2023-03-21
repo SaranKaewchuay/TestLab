c1982af59449a55b87b1ba24c9ab8a0d7a3863a2
SaranKaewchuay <saranyeen@gmail.com>
2023-03-21 21:07:21 +0700

    test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from .tests.function import display_month
 
 app = FastAPI()
 
@@ -7,19 +8,35 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World1"}
 
+@app.get("/")
+def AAAA():
+    input_number = input()
+    try:
+        try:
+            input_number = int(input_number)
+        except:
+            input_number = float(input_number)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    except:
+        input_number = str(input_number)
 
-@app.get("/hello/{name}")
-def read_name(name: str = None):
-    return {"hello": name}
+    result = display_month(input_number)
+    print(result)
 
 
-@app.get("/test")
-def read_name():
-    return {"Hello": "World2"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
+
+# @app.get("/hello/{name}")
+# def read_name(name: str = None):
+#     return {"hello": name}
+
+
+# @app.get("/test")
+# def read_name():
+#     return {"Hello": "World2"}
 
 
 handler = Mangum(app)

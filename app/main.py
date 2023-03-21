eef3ceeca58059aa653f19c4eda902a66eea7b8d
SaranKaewchuay <saranyeen@gmail.com>
2023-03-21 21:26:30 +0700

    b

--- app/main.py
+++ app/main.py
@@ -7,18 +7,18 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
 
-@app.get("/hello/{name}")
-def read_name(name: str = None):
-    return {"hello": name}
+# @app.get("/hello/{name}")
+# def read_name(name: str = None):
+#     return {"hello": name}
 
 
-@app.get("/test")
-def read_name():
-    return {"Hello": "World2"}
+# @app.get("/test")
+# def read_name():
+#     return {"Hello": "World2"}
 
 
 handler = Mangum(app)
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,18 +1,18 @@
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 
-from app.main import app
+# from app.main import app
 
-client = TestClient(app)
+# client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+# def test_read_main():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"Hello": "World"}
     
-def test_hello():
-    response = client.get("/test")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World2"}
+# def test_hello():
+#     response = client.get("/test")
+#     assert response.status_code == 200
+#     assert response.json() == {"Hello": "World2"}
 
 
